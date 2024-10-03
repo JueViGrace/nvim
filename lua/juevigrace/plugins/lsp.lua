61a99e7479d520c180139933131bcd59bd3e1f06
Daniel Zabala <danielzabalanava@gmail.com>
2024-10-03 12:51:54 -0400

    lsp dependencies

--- lua/juevigrace/plugins/lsp.lua
+++ lua/juevigrace/plugins/lsp.lua
@@ -1,25 +1,27 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "j-hui/fidget.nvim",
-        },
-        config = function()
-            vim.diagnostic.config({
-                -- update_in_insert = true,
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                },
-            })
-        end
-    },
-    {
-        "j-hui/fidget.nvim",
-        opts = {}
-    },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"j-hui/fidget.nvim",
+		},
+		config = function()
+			vim.diagnostic.config({
+				-- update_in_insert = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		opts = {},
+	},
 }
