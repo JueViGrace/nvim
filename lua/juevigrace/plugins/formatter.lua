5be2777a8c529cce5ff8e5c18d9aa10fea425f50
Daniel Zabala <danielzabalanava@gmail.com>
2024-10-08 14:20:39 -0400

    prettier formatter

--- lua/juevigrace/plugins/formatter.lua
+++ lua/juevigrace/plugins/formatter.lua
@@ -5,12 +5,15 @@ return {
 		local conform = require("conform")
 
 		conform.setup({
+			notify_on_error = true,
+			log_level = vim.log.levels.DEBUG,
 			formatters_by_ft = {
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				javascriptreact = { "prettier" },
 				typescriptreact = { "prettier" },
 				svelte = { "prettier" },
+				-- astro = { "prettier" },
 				css = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
@@ -27,6 +30,26 @@ return {
 				lsp_fallback = true,
 				async = false,
 				timeout_ms = 500,
+			},
+			formatters = {
+				prettier = {
+					stdin = true,
+					command = "prettier",
+					args = {
+						"--no-color",
+						"--stdin-filepath",
+						vim.api.nvim_buf_get_name(0),
+					},
+					cwd = require("conform.util").root_file({
+						".prettierrc.json",
+						".prettierrc",
+						".prettierrc.mjs",
+						"prettier.config.js",
+					}),
+					inherit = true,
+					prepend_args = { "--use-tabs" },
+					append_args = { "--trailing-comma" },
+				},
 			},
 		})
 
