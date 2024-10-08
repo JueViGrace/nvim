dfb5e9a95ff8e2dc7808a9dd1cd0c143c303fbef
Daniel Zabala <danielzabalanava@gmail.com>
2024-10-08 16:20:56 -0400

    colors

--- lua/juevigrace/plugins/colors.lua
+++ lua/juevigrace/plugins/colors.lua
@@ -71,7 +71,6 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		priority = 1000,
 		config = function()
 			require("catppuccin").setup({
 				flavour = "mocha", -- latte, frappe, macchiato, mocha
@@ -81,7 +80,7 @@ return {
 				},
 				transparent_background = true, -- disables setting the background color.
 				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+				term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
 				dim_inactive = {
 					enabled = false, -- dims the background color of inactive window
 					shade = "dark",
@@ -94,8 +93,8 @@ return {
 					comments = { "italic" }, -- Change the style of comments
 					conditionals = { "italic" },
 					loops = {},
-					functions = {},
-					keywords = {},
+					functions = { "italic" },
+					keywords = { "italic" },
 					strings = {},
 					variables = {},
 					numbers = {},
@@ -105,19 +104,42 @@ return {
 					operators = {},
 					-- miscs = {}, -- Uncomment to turn off hard-coded styles
 				},
-				color_overrides = {},
-				custom_highlights = {},
-				default_integrations = true,
 				integrations = {
-					cmp = true,
-					gitsigns = true,
 					nvimtree = true,
-					treesitter = true,
-					notify = false,
-					mini = {
+					aerial = true,
+					alpha = true,
+					cmp = true,
+					dashboard = true,
+					flash = true,
+					grug_far = true,
+					gitsigns = true,
+					headlines = true,
+					illuminate = true,
+					indent_blankline = { enabled = true },
+					leap = true,
+					lsp_trouble = true,
+					mason = true,
+					markdown = true,
+					mini = true,
+					native_lsp = {
 						enabled = true,
-						indentscope_color = "",
+						underlines = {
+							errors = { "undercurl" },
+							hints = { "undercurl" },
+							warnings = { "undercurl" },
+							information = { "undercurl" },
+						},
 					},
+					navic = { enabled = true, custom_bg = "lualine" },
+					neotest = true,
+					neotree = true,
+					noice = true,
+					notify = true,
+					semantic_tokens = true,
+					telescope = true,
+					treesitter = true,
+					treesitter_context = true,
+					which_key = true,
 					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 				},
 			})
